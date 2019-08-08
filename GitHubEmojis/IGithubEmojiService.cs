efead615bfc27947a557c2160ab407d3839a4aa1
Julian88V <juliangoudy1@yahoo.com>
2019-08-08 17:13:45 -0600

    adding emoji proj

---
+++ GitHubEmojis/IGithubEmojiService.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GitHubEmojis
+{
+    public interface IGithubEmojiService
+    {
+
+        Task<IList<Emoji>> GetEmojis();
+        IList<Emoji> GetEmojisFrom(string content);
+    }
+}
---
+++ GitHubEmojis/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GitHubEmojis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private IGithubEmojiService _emojiService;
+
+        public IndexModel(IGithubEmojiService emojiService)
+        {
+            _emojiService = emojiService;
+        }
+
+        public IList<Emoji> Emojis
+        {
+            get;
+            set;
+        }
+
+        public async Task OnGet()
+        {
+            Emojis = await _emojiService.GetEmojis();
+        }
+    }
+}
